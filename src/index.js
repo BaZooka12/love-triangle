4ae6d8d4e69e482da02a48c980f962a3997b3e3c
Vlad Ilkovec <darkbe149l@mail.ru>
2018-02-20 23:14:33 +0300

    Love triangles solution

--- src/index.js
+++ src/index.js
@@ -3,5 +3,17 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let count = 0;
+  let jango = [];
+  for(let i = 0; i < preferences.length; i++) {
+    let a = preferences[i-1];
+    let b = preferences[a-1];
+    let c = preferences[b-1];
+    
+    if(c === i && a !== b && !jango.includes(a)) {
+      jango.push(a,b,c);
+      count++;
+    } 
+  }
+  return count;
 };
